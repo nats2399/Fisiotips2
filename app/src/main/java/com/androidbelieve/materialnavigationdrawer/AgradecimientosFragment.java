efffc49f903e553b80e8262034841feac991a1ee
Nathalie Ayala Santana <nathalieaysa@gmail.com>
2016-05-15 02:06:37 -0500

    Commit7

--- app/src/main/java/com/androidbelieve/materialnavigationdrawer/AgradecimientosFragment.java
+++ app/src/main/java/com/androidbelieve/materialnavigationdrawer/AgradecimientosFragment.java
@@ -23,7 +23,7 @@ public class AgradecimientosFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
 
-        return inflater.inflate(R.layout.fragment_intro,null);
+        return inflater.inflate(R.layout.fragment_agradecimientos,null);
 
     }
 
