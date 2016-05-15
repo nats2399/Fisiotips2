3b9b75084656cd3ca91ba84870822e916936670b
Nathalie Ayala Santana <nathalieaysa@gmail.com>
2016-05-15 00:04:30 -0500

    Commit4

---
+++ app/src/main/java/com/androidbelieve/materialnavigationdrawer/AgradecimientosFragment.java
@@ -0,0 +1,33 @@
+package com.androidbelieve.materialnavigationdrawer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link AgradecimientosFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link AgradecimientosFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AgradecimientosFragment extends android.support.v4.app.Fragment {
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+
+        return inflater.inflate(R.layout.fragment_intro,null);
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/androidbelieve/materialnavigationdrawer/WelcomeFragment.java
+++ app/src/main/java/com/androidbelieve/materialnavigationdrawer/WelcomeFragment.java
@@ -1,25 +1,23 @@
 package com.androidbelieve.materialnavigationdrawer;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class IntroFragment extends android.support.v4.app.Fragment {
+public class WelcomeFragment extends android.support.v4.app.Fragment {
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        final View view = inflater.inflate(R.layout.fragment_intro, container, false);
 
         return inflater.inflate(R.layout.fragment_intro,null);
 
     }
-
-
-
 
 }
